06f684cfb1d1e177bd05d0c3d70a190718bd41df
Usuario <Usuario@S4-18>
2022-03-14 21:36:01 -0300

    Primer commit de la clase algoritmos complejos

---
+++ ALC/src/laboratorioALC/Calculadora.java
@@ -0,0 +1,23 @@
+package laboratorioALC;
+
+public class Calculadora {
+
+    public    Integer var;
+    private   Integer var2;
+    protected Integer var3;
+              Integer var4;
+    public Integer sumar() {
+        var  = 123;
+        var2 = 456;
+        var4 = 789;
+        return null;
+    }
+
+    public String mensaje() {
+        return "Mensaje";
+    }
+    
+    private void abc() {
+        
+    }
+}
---
+++ ALC/src/laboratorioALC/Ejemplo.java
@@ -0,0 +1,12 @@
+package laboratorioALC;
+
+public class Ejemplo {
+
+    private Calculadora calc;
+
+    public Ejemplo() {
+        calc = new Calculadora();
+        calc.sumar();
+    }
+
+}
