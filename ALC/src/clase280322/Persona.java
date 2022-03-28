ee1a76663d98e8bc8712dd7272021c36266bc3a5
Usuario <Usuario@S4-18>
2022-03-28 19:09:57 -0400

    primer commit de la clase 28-03-22

---
+++ ALC/src/clase280322/Persona.java
@@ -0,0 +1,65 @@
+package clase280322;
+
+import java.util.Date;
+
+public class Persona {
+    private Integer id;
+    private Integer nroDoc;
+    private String nombre;
+    private String apellido;
+    private Date fecNac;
+    private String sexo;
+
+    public Persona() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNroDoc() {
+        return nroDoc;
+    }
+
+    public void setNroDoc(Integer nroDoc) {
+        this.nroDoc = nroDoc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFecNac() {
+        return fecNac;
+    }
+
+    public void setFecNac(Date fecNac) {
+        this.fecNac = fecNac;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+}
